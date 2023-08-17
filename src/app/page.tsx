6650a600762c94e10e08fcbd73d3df02e6f23d84
wesleywil <wesleywilsonti@gmail.com>
2023-08-16 22:13:19 -0300

    updated style of the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,22 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        <h1>Book Library</h1>
-        <h2>
+    <main className="min-h-screen flex  flex-col items-center justify-center p-24">
+      <div className="w-2/3 flex flex-col gap-2 justify-center items-center">
+        <h1 className="text-6xl">Book Library</h1>
+        <h2 className="text-xl text-center">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, quae
           laudantium similique quos nihil nostrum mollitia doloremque incidunt
           adipisci sed voluptates, ex vel ad officia veritatis. Soluta in velit
           nemo.
         </h2>
+        <div className="mt-4 flex flex-col gap-4 justify-center text-2xl">
+          <button className="px-2 py-1 bg-red-500 hover:bg-red-700 rounded">
+            Login
+          </button>
+          <button className="px-2 py-1 bg-red-500 hover:bg-red-700 rounded">
+            Create Account
+          </button>
+        </div>
       </div>
     </main>
   );
