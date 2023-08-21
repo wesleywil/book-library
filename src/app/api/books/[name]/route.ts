e265eb97c4b7f35e0e7e495b559d163dd8258082
wesleywil <wesleywilsonti@gmail.com>
2023-08-21 14:52:14 -0300

    updated books api by filtering only the necessary data comming from the api

--- src/app/api/books/[name]/route.ts
+++ src/app/api/books/[name]/route.ts
@@ -6,10 +6,24 @@ export async function GET(request:Request, {params}:{params:{name:string}}){
         const req = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${params.name}`,{
             cache:"default"
         });
-        const books:Book[] = await req.json();
-        return NextResponse.json({
-            books:books
-        })
+        const responseData:any = await req.json();
+       
+       const books:Book[] = responseData.items.map((book:any)=>({
+        id: book.id,
+        etag: book.etag,
+        selfLink: book.selfLink,
+        volumeInfo: {
+          title: book.volumeInfo.title,
+          authors: book.volumeInfo.authors,
+          publishedDate: book.volumeInfo.publishedDate,
+          description: book.volumeInfo.description,
+          industryIdentifiers: book.volumeInfo.industryIdentifiers,
+          pageCount: book.volumeInfo.pageCount,
+          categories: book.volumeInfo.categories,
+          imageLinks: book.volumeInfo.imageLinks
+        }
+       }));
+   return NextResponse.json(books)
     }catch(error){
         return NextResponse.json({
             error: `An error occurred while trying to update the expense - ${error}`,
